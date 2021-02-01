ea8b8f1c21ebf877cb37fbf09d43b5ef8e6b72e4
Ravindra Shinde <neelravi@gmail.com>
2021-02-01 10:02:49 +0100

    layout pushed

---
+++ utils/pychampread.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021, the TREX development team
+#
+# This file is part of TREX (http://trex-coe.eu) and is distributed under
+# the terms of the [] License.
+
+
+import logging
+import sys
+import numpy as np
+
+
+class pyread(object):
+    """Main class for reading the quantum chemistry output and log files"""
+
+
+
+
---
+++ utils/pychampwrite.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021, the TREX development team
+#
+# This file is part of TREX (http://trex-coe.eu) and is distributed under
+# the terms of the [] License.
+
+
+import logging
+import sys
+import numpy as np
+
+
+class pywrite(object):
+    """Main class for writing the output in desired formats"""
+
+
+
+
